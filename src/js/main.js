cc5ef37395e0c47a276881a4cfb5fd9803276769
Mark Durrant <mark.j.durrant@gmail.com>
2014-12-24 14:43:05 +0000

    rm jquery

--- src/js/main.js
+++ src/js/main.js
@@ -20,6 +20,7 @@
   // enable mouse/touch view controls
   new THREE.OrbitControls( camera, renderer.domElement );
 
+  // set up background
   var background = {};
       background.geometry = new THREE.SphereGeometry( 50, 16, 16 );
       background.material = new THREE.MeshBasicMaterial({
@@ -27,6 +28,7 @@
         side: THREE.DoubleSide
       });
       background.mesh = new THREE.Mesh( background.geometry, background.material );
+      scene.add( background.mesh );
 
   var wireframe = new THREE.MeshBasicMaterial({
     color: 0xffffff,
@@ -39,15 +41,15 @@
   var gridGeo = new THREE.Geometry(); // create empty geometry
 
   gridGeo.vertices.push( // add vertices to gridGeo
-    new THREE.Vector3( -1, -1, Math.random() ),
-    new THREE.Vector3( 0, -1, Math.random() ),
-    new THREE.Vector3( 1, -1, Math.random() ),
-    new THREE.Vector3( -1, 0, Math.random() ),
-    new THREE.Vector3( 0, 0, Math.random() ),
-    new THREE.Vector3( 1, 0, Math.random() ),
-    new THREE.Vector3( -1, 1, Math.random() ),
-    new THREE.Vector3( 0, 1, Math.random() ),
-    new THREE.Vector3( 1, 1, Math.random() )
+    new THREE.Vector3( -1, -1, 0 ),
+    new THREE.Vector3( 0, -1, 0 ),
+    new THREE.Vector3( 1, -1, 0 ),
+    new THREE.Vector3( -1, 0, 0 ),
+    new THREE.Vector3( 0, 0, 0 ),
+    new THREE.Vector3( 1, 0, 0 ),
+    new THREE.Vector3( -1, 1, 0 ),
+    new THREE.Vector3( 0, 1, 0 ),
+    new THREE.Vector3( 1, 1, 0 )
   );
 
   gridGeo.faces.push( // create faces from vertices for grid
@@ -66,6 +68,6 @@
   var grid = new THREE.Mesh( gridGeo, wireframe );
 
   // add objects to scene
-  scene.add( grid, background.mesh );
+  scene.add( grid );
 
 }()); // end 'use strict'
